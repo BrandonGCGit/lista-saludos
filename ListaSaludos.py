fba0b770528d8cf4656bb00c54d3b61fb3659622
Brandon_GIT <brandgc1516@gmail.com>
2022-07-31 00:10:21 -0600

    Se agregaron despedidas al archivo

--- ListaSaludos.py
+++ ListaSaludos.py
@@ -1,3 +1,5 @@
 print("Lista de saludos")
 saludos = ['HI', 'GOOD MORNING', 'GOOD AFTERNOON', 'GOOD EVENING', 'HELLO']
 print(saludos)
+despedidas= ['BYE', 'GOOD BYE','SEE YOU','SEE YOU LATER']
+print(despedidas)
