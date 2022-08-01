ba6f4272fa3d908937efd65242f60702d6f456e3
Brandon_GIT <brandgc1516@gmail.com>
2022-07-31 18:58:59 -0600

    Se limino esa vaina

--- ListaSaludos.py
+++ ListaSaludos.py
@@ -3,4 +3,3 @@ saludos = ['HI', 'GOOD MORNING', 'GOOD AFTERNOON', 'GOOD EVENING', 'HELLO']
 print(saludos)
 despedidas= ['BYE', 'GOOD BYE','SEE YOU','SEE YOU LATER']
 print(despedidas)
-#Aquin termina mi codigo
